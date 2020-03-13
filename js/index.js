670b68f6a36c2328f2b24981c1aaaa1dd16382f6
aland1013 <adam.land@gmail.com>
2020-03-12 21:35:27 -0600

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+// Change title to red on scroll
+const title = document.querySelector('.nav-container h1');
+
+window.addEventListener('scroll', () => {
+  title.style.color = 'red';
+});
+
+// Change nav text color and background on hover
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+    event.target.style.background = 'yellow';
+  });
+  link.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+    event.target.style.background = 'white';
+  });
+});
+
+// Make header image larger on hover
+const headerImg = document.querySelector('.intro img');
+
+headerImg.addEventListener('mouseenter', (event) => {
+  event.target.style.transform = 'scale(1.2)';
+  event.target.style.transition = 'all 0.3s';
+});
+
+headerImg.addEventListener('mouseleave', (event) => {
+  event.target.style.transform = 'scale(1)';
+});
+
