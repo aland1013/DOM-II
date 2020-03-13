8feb4a7c341f074effd06305131b8e7799c0858e
aland1013 <adam.land@gmail.com>
2020-03-12 22:01:19 -0600

    finished adding event listeners

--- js/index.js
+++ js/index.js
@@ -31,3 +31,41 @@ headerImg.addEventListener('mouseleave', (event) => {
   event.target.style.transform = 'scale(1)';
 });
 
+// Change paragraph text color to pink when clicked
+const pText = document.querySelectorAll('p');
+
+pText.forEach(p => {
+  p.addEventListener('click', (event) => {
+    event.target.style.color = 'pink';
+  });
+});
+
+// Change header font when double clicked
+const headers = document.querySelectorAll('h2');
+
+headers.forEach(header => {
+  header.addEventListener('dblclick', (event) => {
+    event.target.style.fontFamily = 'Arial Black';
+  });
+});
+
+// Make body background color black while a key is pressed
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', (event) => {
+  event.target.style.background = 'black';
+});
+
+body.addEventListener('keyup', (event) => {
+  event.target.style.background = 'white';
+});
+
+// Make the images change on resize
+
+images = document.querySelectorAll('img');
+
+window.addEventListener('resize', () => {
+  images.forEach(img => {
+    img.style.width = '50%';
+  });
+});
