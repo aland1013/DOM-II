a50a013503bb8207d0f67f9985cf5e51b6bd8e76
aland1013 <adam.land@gmail.com>
2020-03-13 19:52:33 -0700

    refactored event listener

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,17 +1,10 @@
 const blocks = document.querySelectorAll('.block');
-blocks.forEach((block, index) => {
-  block.style.order = index;
-});
-
 blocks.forEach((block, index, arr) => {
+  block.style.order = index;
   block.addEventListener('click', (event) => {
     event.target.style.order = 0;
-    console.log(event.target.style.order);
-
     for (let i = 0; i < index; i++) {
       arr[i].style.order++;
     }
   });
-})
-  
-console.log(blocks);
+});
