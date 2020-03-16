b5912a2defc42e0db7dfa6fad087954a3c82763f
aland1013 <adam.land@gmail.com>
2020-03-16 15:48:06 -0400

    completed basic stretch project

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,11 +1,33 @@
 const blocks = document.querySelectorAll('.block');
-blocks.forEach((block) => {
+
+blocks.forEach((block, index) => {
+  block.style.position = 'absolute';
+  block.style.top = index * 150 + 'px';
+
   block.addEventListener('click', (event) => {
     event.target.remove();
     document.querySelector('.blocks').insertBefore(event.target, document.querySelector('.block'));
-  });
-  Draggable.create(block, {
-      type: 'x',
-      lockAxis: true
+
+    document.querySelectorAll('.block').forEach((block, index) => {
+      block.style.top = index * 150 + 'px';
     });
+  });
+
+  let intervalId;
+  let pos = 0;
+
+  block.addEventListener('mousedown', (event) => {
+    intervalId = window.setInterval(() => {
+      pos++;
+      event.target.style.left = pos + 'px';
+    }, 5); 
+  });
+
+  block.addEventListener('mouseup', () => {
+    clearInterval(intervalId);
+  });
+
+  block.addEventListener('mouseout', () => {
+    clearInterval(intervalId);
+  })
 });
